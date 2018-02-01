e8c71c116eed418d8c26a0fa65a95bfd5ff48f0d
daniel kim <daniel.c.kim@live.com>
2018-02-01 13:36:23 -0800

    almost done

--- js/cart.js
+++ js/cart.js
@@ -1,3 +1,62 @@
+function Cart(cartTable) {
+    var cart = this.loadCookie();
+    var contentEnd = cartTable.querySelector("#cart_content_end");
+    var cartItemTemplate = cartTable.querySelector("#cart_item_template");
+
+    cart.forEach(function(cartItem) {
+        var tr = this.createNewTr(cartItem, cartItemTemplate);
+        contentEnd.parentNode.insertBefore(tr, contentEnd);
+    }, this);
+
+    
+}
+Cart.prototype.createNewTr = function(cartItem, cartItemTemplate) {
+    var tr = cartItemTemplate.cloneNode(true);
+    var thumb = (cartItem.type === "Sourdough Baguette") ? "images/common/thumbs_baguette_01.jpg" : "images/common/thumbs_boule_01.jpg";
+
+    tr.querySelector(".thumbs img").setAttribute("src", thumb);
+    tr.querySelector("button").innerText = cartItem.type;
+
+    cartItem.flavors.forEach(function(flv) {
+        var flvClass = flv.replace(" ","").toLowerCase();
+        var btn = document.createElement("button");
+        btn.className = "btn btn-sm btn-bg active";
+        btn.classList.add("btn-"+flvClass);
+        btn.innerText = flv;
+        btn.style.marginRight = "3px";
+
+        tr.querySelector("td").appendChild(btn);
+    })
+
+
+    tr.querySelector("td:nth-child(2)").innerText = this.calculateUnitPrice(cartItem).toFixed(2);
+    tr.querySelector(".quantity-input").value = cartItem.num;
+    tr.querySelector("td:nth-child(4) span").innerText = (this.calculateUnitPrice(cartItem) * cartItem.num).toFixed(2);
+    
+    return tr;
+}
+Cart.prototype.loadCookie = function() {
+    var cartCookie = 
+        document.cookie.split(";")
+            .map(function(x) {
+                return x.trimLeft()
+            }).filter( function(x) {
+                return x.startsWith("cart=")
+            }).map( function(x) {
+                return x.replace("cart=","");
+            })[0] || "[]";
+
+    cart = JSON.parse(cartCookie);
+    cart = cart.map(function(el) {
+        return JSON.parse(el);
+    })
+    return cart;
+}
+Cart.prototype.calculateUnitPrice = function(cartItem) {
+    return 3.5 + cartItem.flavors.length * 0.5;
+}
+
+
 function CartItem(type, flavors, num) {
     this.type = type;
     this.flavors = flavors;
