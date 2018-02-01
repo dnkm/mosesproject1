6735d716d9ce4c946021ef0eff09e0a6cc877f5e
daniel kim <daniel.c.kim@live.com>
2018-02-01 01:21:43 -0800

    좀 했음

--- js/cart.js
+++ js/cart.js
@@ -1,42 +1,77 @@
-let option3;
+function CartItem(type, flavors, num) {
+    this.type = type;
+    this.flavors = flavors;
+    this.num = num;
 
-window.addEventListener('load', () => {
-    option3 = document.querySelector("#option3");
+    this.getPrice = function() {
+        return 3.5 + this.flavors.length * 0.5;
+    }
+}
 
-    activateThumbnails();
+function OrderPage(orderPage) {
+    this.orderPage = orderPage;
+    this.flavorOptions = orderPage.querySelector(".dkcart_flavor_options");
+    this.quantityInput = orderPage.querySelector("#quantity");
+    
 
-    document.querySelector("#r-basic").addEventListener('click', () => {
-        option3.classList.remove("on");
-        updateCart();
-    });
+    var that = this;
+    orderPage.querySelectorAll("input, button").forEach(function(tag) {
+        tag.addEventListener('click', function(ev) { 
+            that.onInputClick(ev);
+        });
+    })
 
-    document.querySelector("#r-flavor").addEventListener('click', () => {
-        option3.classList.add("on");
-    });
-
-    document.querySelectorAll("#option3 input").forEach(input => {
-        input.addEventListener('click', updateCart);
-    });
-})
-
-function activateThumbnails() {
-    let prevDiv = document.querySelector(".goods-prview");
-    let imgHolder = prevDiv.querySelector("img");
-    prevDiv.querySelectorAll("li img").forEach((img,i) => {
+    this.activateThumbnails();
+}
+OrderPage.prototype.activateThumbnails = function() {
+    var prevDiv = this.orderPage.querySelector(".goods-prview");
+    var imgHolder = this.orderPage.querySelector("img");
+    prevDiv.querySelectorAll("li img").forEach(function(img,i) {
         img.addEventListener('click', () => {
             imgHolder.setAttribute('src', img.dataset.img);
         })
     })
 }
+OrderPage.prototype.onInputClick = function(ev) {
+    var tag = ev.target;
 
-function resetOptions() {
+    switch(tag.id) {
+        case "quantity_less":
+            console.log(this.quantityInput.value)
+            this.quantityInput.value = Math.max(parseInt(this.quantityInput.value) - 1, 0);
+            break;
+        case "quantity_more":
+            this.quantityInput.value = parseInt(this.quantityInput.value) + 1;
+            break;
+        case "r-flavor":
+            this.flavorOptions.classList.add("on");
+            break;
+        case "r-basic":
+            this.flavorOptions.classList.remove("on");
+            break;
+    }
+    
+    
+    var numOptions = this.flavorOptions.querySelectorAll("input:checked").length;
+    if (numOptions > 2) {
+        ev.preventDefault();
+        return false;
+    }
 
+    var quantity = parseInt(this.orderPage.querySelector("#quantity").value);
+
+    var price = 3.5 + 0.5 * ((this.flavorOptions.classList.contains("on")) ? numOptions : 0) * quantity;
+    this.orderPage.querySelector("#final_price").innerText = "$" + price.toFixed(2);
 }
 
-function updateCart() {
-    let numOptions = (option3.classList.contains("on")) ? document.querySelectorAll("#option3 input:checked").length : 0;
 
-    let price = 3.5 + 0.5 * numOptions;
+window.addEventListener('load', () => {
+    var cart = [];
 
-    document.querySelector("#final_price").innerText = "$" + price.toFixed(2);
-}
+    var orderPage = document.querySelector(".dkcart_order")
+    if (orderPage) {
+        new OrderPage(orderPage);
+    }
+
+})
+
